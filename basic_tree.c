8b4df8ff14568b853f8bee76aef48d6774969069
Mikko Saari <mikko.l.saari@tuni.fi>
2024-07-15 11:59:57 +0300

    straight-line program-abstract-tree_init

---
+++ basic_tree.c
@@ -0,0 +1,8 @@
+//typedef->Grammar
+typedef char* string; //-> string 
+typedef struct A_stm_ *A_stm; // statement
+typedef struct A_exp_ *A_exp; //expression
+typedef struct A_expList_ *A_expLIst; //expressionlist
+typedef enum{A_plus,A_minus,A_mul,A_div} A_binop;
+
+struct A_stm_{};
