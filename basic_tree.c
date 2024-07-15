ec9e04bea4b58b6269a8f7fd1886709942009485
Mikko Saari <mikko.l.saari@tuni.fi>
2024-07-15 12:05:57 +0300

    straight-line program-abstract-tree_more

--- basic_tree.c
+++ basic_tree.c
@@ -5,4 +5,17 @@ typedef struct A_exp_ *A_exp; //expression
 typedef struct A_expList_ *A_expLIst; //expressionlist
 typedef enum{A_plus,A_minus,A_mul,A_div} A_binop;
 
-struct A_stm_{};
+struct A_stm_{}; //for statements
+A_stm A_CompoundStm();
+A_stm A_AssignStm();
+A_stm A_PrintStm();
+
+struct A_exp_{}; // for expressions
+
+A_exp A_IdExp();
+A_exp A_NumExp();
+A_exp A_OpExp();
+A_exp A_EseqExp();
+
+struct A_expLIst_{};//for list of expressions
+
