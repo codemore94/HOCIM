308d42c099fa104bed186282b41962b0e3b67f8e
Mikko Saari <mikko.l.saari@tuni.fi>
2025-09-25 01:03:15 +0300

    check_in_main

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <ctype.h>
+#include <stdio.h>
 
-int main(int argc, char* argv[]{
-    char* progname=argv[0];
-    yyparse();
+int lineno;
+char* progname;
+void warning(char* s, char* t){
+  //fprintf("%s,%s",(FILE *__restrict)progname,(FILE*__restrict)t);
+	if(t){ //C-style error checking, pointer t to char is NULL-->FALSE, otherwise true
+	  //fprintf("%s",t);
+	}		
+	//fprintf(stderr,"%d\n",lineno);
+			//C-style error checking, pointer t to char is NULL-->FALSE, otherwise true
 }
 
 void yyparse(){
 	int lineno; //check if defined in correct place
 	int c; //check if the type should be char
 
-	if((c= getchar()) ==´ ´ || c== ´\t´){
+	if((c= getchar()) ==' ' || c== '\t'){
 	
  	} //check if input is either empty or tab
 	
@@ -17,12 +24,12 @@ void yyparse(){
 		return 0;
 	} //check if input is End-Of-File
 
-	if(c == ´ . ´  || isdigit(c)){
+	if(c == ' . '  || isdigit(c)){
 		ungetc(c,stdin);
 	} //check if input is either point or a digit 
 
 		
-	if(c == ´ \n ´){
+	if(c == ' \n '){
 		lineno++;
 	} //check if input is either point or a digit 
 	return c;
@@ -32,11 +39,7 @@ void yyerror(char* s){
 	warning(s,(char*)0);
 }
 
-void warning(char* s, char* t){
-	fprintf("%s,%s",progname,t);
-	if(t){ //C-style error checking, pointer t to char is NULL-->FALSE, otherwise true
-		fprintf("%s",t);
-	}		
-	fprintf(stderr,"%d\n",lineno);
-			//C-style error checking, pointer t to char is NULL-->FALSE, otherwise true
+int main(int argc, char* argv[]){
+    progname=argv[0];
+    yyparse();
 }
